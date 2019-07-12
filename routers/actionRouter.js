d9ebf0c0a16de33f2270f19770c9bd4466712154
Becheru888 <becheru93@gmail.com>
2019-07-12 10:01:43 +0100

    actionRouter completed

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -15,14 +15,81 @@ router.get("/", (req, res) => {
       });
   });
 
+  router.post("/", validateProjectId, (req, res) => {
+    const newAction = {
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes
+    };
+    actionsdb.insert(newAction)
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "error occured when trying to add an action" });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    actionsdb.get(id)
+      .then(action => {
+        if (action) {
+          res.status(200).json({
+            action
+          });
+        } else {
+          res.status(201).json({ message: "sorry no action" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ err, message: "Unable to retrieve action" });
+      });
+  });
+
+  router.put("/:id", validateActionId, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    actionsdb.update(id, changes)
+      .then(UpdateAction => {
+        if (UpdateAction) {
+          res.status(200).json(UpdateAction);
+        } else {
+          res.status(404).json({ message: " project does not exist" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: "another error"
+        });
+      });
+  });
+
+  router.delete("/:id", validateActionId, (req, res) => {
+    const id = req.params.id;
+    actionsdb.remove(id)
+      .then(response => {
+        res.status(200).json({
+          message: "action deleted"
+        });
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: "another error"
+        });
+      });
+  });
+
 
 // Costum Middleware
 
 
   function validateActionId(req, res, next) {
     const id = req.params.id;
-    actionsdb
-      .get(id).then(action => {
+    actionsdb.get(id)
+    .then(action => {
         if (action) {
           req.action = action;
           next();
